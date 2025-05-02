6d0ebb13d9e82f272c602090750e4e66e7232d19
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 15:38:36 +0300

    #2 Add super entity

---
+++ src/main/java/com/huseyinsarsilmaz/lms/model/entity/LmsEntity.java
@@ -0,0 +1,37 @@
+package com.huseyinsarsilmaz.lms.model.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class LmsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
