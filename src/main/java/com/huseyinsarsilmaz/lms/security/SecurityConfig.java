e0638df80e81b323584b5417724e52756e84fe73
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 16:00:48 +0300

    #4 Add security config

---
+++ src/main/java/com/huseyinsarsilmaz/lms/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.huseyinsarsilmaz.lms.security;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huseyinsarsilmaz.lms.model.dto.response.ApiResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    private void handleExceptionInFilterChain(HttpServletRequest request, HttpServletResponse response,
+            RuntimeException exception, int responseCode) throws IOException {
+        ApiResponse apiResponse = new ApiResponse(false,
+                "Unauthorized: " + exception.getMessage(), null);
+        response.setStatus(responseCode);
+        response.setContentType("application/json");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(apiResponse));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/register").permitAll()
+                .anyRequest().authenticated())
+                .sessionManagement(sess -> sess
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint((request, response,
+                                authException) -> handleExceptionInFilterChain(request,
+                                        response, authException,
+                                        HttpServletResponse.SC_UNAUTHORIZED))
+                        .accessDeniedHandler((request, response,
+                                accessDeniedException) -> handleExceptionInFilterChain(
+                                        request,
+                                        response, accessDeniedException,
+                                        HttpServletResponse.SC_FORBIDDEN)))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
