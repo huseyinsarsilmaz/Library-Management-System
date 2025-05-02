5a0a923c85ccbacdf98bd8426d1b2ca04b20103c
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 15:47:00 +0300

    #3 Add register method

--- src/main/java/com/huseyinsarsilmaz/lms/service/UserService.java
+++ src/main/java/com/huseyinsarsilmaz/lms/service/UserService.java
@@ -1,7 +1,11 @@
 package com.huseyinsarsilmaz.lms.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.huseyinsarsilmaz.lms.model.dto.request.RegisterRequest;
+import com.huseyinsarsilmaz.lms.model.entity.User;
 import com.huseyinsarsilmaz.lms.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,5 +14,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public User register(RegisterRequest req) {
+        // TODO Tightly coupled will be handled
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        req.setPassword(encoder.encode(req.getPassword()));
+
+        User newUser = User.builder()
+                .email(req.getEmail())
+                .password(req.getPassword())
+                .name(req.getName())
+                .surname(req.getSurname())
+                .phoneNumber(req.getPhoneNumber())
+                .build();
+
+        return userRepository.save(newUser);
+    }
 
 }
