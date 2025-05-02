722b82c4db2546c9f335dad02a919bbab16820bc
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 16:08:27 +0300

    #5 Move register to auth controller

---
+++ src/main/java/com/huseyinsarsilmaz/lms/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.huseyinsarsilmaz.lms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huseyinsarsilmaz.lms.model.dto.request.RegisterRequest;
+import com.huseyinsarsilmaz.lms.model.dto.response.ApiResponse;
+import com.huseyinsarsilmaz.lms.model.dto.response.RegisterResponse;
+import com.huseyinsarsilmaz.lms.model.entity.User;
+import com.huseyinsarsilmaz.lms.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterRequest req) {
+
+        User newUser = userService.register(req);
+        ApiResponse response = new ApiResponse(true, "User successfully registered", new RegisterResponse(newUser));
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+    }
+
+}
--- src/main/java/com/huseyinsarsilmaz/lms/controller/UserController.java
+++ src/main/java/com/huseyinsarsilmaz/lms/controller/UserController.java
@@ -1,19 +1,10 @@
 package com.huseyinsarsilmaz.lms.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.huseyinsarsilmaz.lms.model.dto.request.RegisterRequest;
-import com.huseyinsarsilmaz.lms.model.dto.response.ApiResponse;
-import com.huseyinsarsilmaz.lms.model.dto.response.RegisterResponse;
-import com.huseyinsarsilmaz.lms.model.entity.User;
 import com.huseyinsarsilmaz.lms.service.UserService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,15 +12,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterRequest req) {
-        userService.isEmailTaken(req.getEmail());
-
-        User newUser = userService.register(req);
-        ApiResponse response = new ApiResponse(true, "User successfully registered", new RegisterResponse(newUser));
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-
-    }
 
 }
