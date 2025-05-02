a124abf1ba1fe262d196fdd8a7898d8804c4e780
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 16:09:25 +0300

    Add login request and response dto

---
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.huseyinsarsilmaz.lms.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 32, message = "Password must be between 8 and 32 characters")
+    private String password;
+}
---
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/response/LoginResponse.java
@@ -0,0 +1,9 @@
+package com.huseyinsarsilmaz.lms.model.dto.response;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+
+    private final String token;
+}
