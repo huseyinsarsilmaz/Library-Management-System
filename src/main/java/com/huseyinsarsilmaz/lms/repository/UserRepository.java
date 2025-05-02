5c3b921edf764e81c4de47cfd488a1d4bc47043d
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 15:44:20 +0300

    #3 Add user repository

---
+++ src/main/java/com/huseyinsarsilmaz/lms/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.huseyinsarsilmaz.lms.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.huseyinsarsilmaz.lms.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
