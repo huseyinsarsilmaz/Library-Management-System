1114edf24a235a8fe8b8c99e991effc3e84454b5
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-02 15:56:37 +0300

    #4 Add custom user details implementation

---
+++ src/main/java/com/huseyinsarsilmaz/lms/security/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package com.huseyinsarsilmaz.lms.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.huseyinsarsilmaz.lms.model.entity.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = Stream.of(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
