3e4ed76b354fad31b40459eddffce80700c1b16d
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-04 01:23:21 +0300

    #7 Add base exception class

---
+++ src/main/java/com/huseyinsarsilmaz/lms/exception/LmsException.java
@@ -0,0 +1,19 @@
+package com.huseyinsarsilmaz.lms.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class LmsException extends RuntimeException {
+    private final String type;
+    private final String[] args;
+    private final HttpStatus httpStatus;
+
+    public LmsException(String messageKey, String[] args, HttpStatus httpStatus) {
+        super();
+        this.type = messageKey;
+        this.args = args;
+        this.httpStatus = httpStatus;
+    }
+}
