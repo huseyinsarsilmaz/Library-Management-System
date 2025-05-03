fee21ccd35cf37bc8cfaf0ed9a747be2f8062e55
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-04 01:38:02 +0300

    #8 Add interface for user service

--- src/main/java/com/huseyinsarsilmaz/lms/service/UserService.java
+++ src/main/java/com/huseyinsarsilmaz/lms/service/UserService.java
@@ -1,56 +1,13 @@
 package com.huseyinsarsilmaz.lms.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import com.huseyinsarsilmaz.lms.exception.AlreadyExistsException;
-import com.huseyinsarsilmaz.lms.exception.NotFoundException;
 import com.huseyinsarsilmaz.lms.model.dto.request.RegisterRequest;
 import com.huseyinsarsilmaz.lms.model.entity.User;
-import com.huseyinsarsilmaz.lms.repository.UserRepository;
 
-import lombok.RequiredArgsConstructor;
+public interface UserService {
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    public void isEmailTaken(String email);
 
-    public void isEmailTaken(String email) {
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new AlreadyExistsException(User.class.getSimpleName(), "email");
-        }
-    }
+    public User register(RegisterRequest req);
 
-    public User register(RegisterRequest req) {
-        req.setPassword(passwordEncoder.encode(req.getPassword()));
-        List<String> roles = new ArrayList<>();
-        roles.add(User.Role.LIBRARIAN.name());
-
-        User newUser = User.builder()
-                .email(req.getEmail())
-                .password(req.getPassword())
-                .name(req.getName())
-                .surname(req.getSurname())
-                .phoneNumber(req.getPhoneNumber())
-                .roles(roles.toString())
-                .build();
-
-        return userRepository.save(newUser);
-    }
-
-    public User getByEmail(String email) {
-        Optional<User> optUser = userRepository.findByEmail(email);
-        if (optUser.isEmpty()) {
-            throw new NotFoundException(User.class.getSimpleName(), "email");
-        }
-
-        return optUser.get();
-    }
-
+    public User getByEmail(String email);
 }
---
+++ src/main/java/com/huseyinsarsilmaz/lms/service/impl/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.huseyinsarsilmaz.lms.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.huseyinsarsilmaz.lms.exception.AlreadyExistsException;
+import com.huseyinsarsilmaz.lms.exception.NotFoundException;
+import com.huseyinsarsilmaz.lms.model.dto.request.RegisterRequest;
+import com.huseyinsarsilmaz.lms.model.entity.User;
+import com.huseyinsarsilmaz.lms.repository.UserRepository;
+import com.huseyinsarsilmaz.lms.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void isEmailTaken(String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new AlreadyExistsException(User.class.getSimpleName(), "email");
+        }
+    }
+
+    public User register(RegisterRequest req) {
+        req.setPassword(passwordEncoder.encode(req.getPassword()));
+        List<String> roles = new ArrayList<>();
+        roles.add(User.Role.LIBRARIAN.name());
+
+        User newUser = User.builder()
+                .email(req.getEmail())
+                .password(req.getPassword())
+                .name(req.getName())
+                .surname(req.getSurname())
+                .phoneNumber(req.getPhoneNumber())
+                .roles(roles.toString())
+                .build();
+
+        return userRepository.save(newUser);
+    }
+
+    public User getByEmail(String email) {
+        Optional<User> optUser = userRepository.findByEmail(email);
+        if (optUser.isEmpty()) {
+            throw new NotFoundException(User.class.getSimpleName(), "email");
+        }
+
+        return optUser.get();
+    }
+
+}
