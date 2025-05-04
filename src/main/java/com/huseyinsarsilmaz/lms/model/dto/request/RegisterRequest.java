a2114dcc16760d389873863cdf6fd2567e0a9e7f
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-04 14:45:11 +0300

    #10 Add phone number validation

--- src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/RegisterRequest.java
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/RegisterRequest.java
@@ -1,11 +1,11 @@
 package com.huseyinsarsilmaz.lms.model.dto.request;
 
 import com.huseyinsarsilmaz.lms.validation.Alphabethical;
+import com.huseyinsarsilmaz.lms.validation.PhoneNumber;
 import com.huseyinsarsilmaz.lms.validation.RequiredField;
 import com.huseyinsarsilmaz.lms.validation.StrSize;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -33,7 +33,7 @@ public class RegisterRequest {
     private String surname;
 
     @RequiredField(entityName = "User", fieldName = "phone number")
-    @Pattern(regexp = "^[+]?[0-9]{10,15}$", message = "Phone number must be valid and contain 10 to 15 digits")
+    @PhoneNumber(entityName = "User", fieldName = "phone number")
     private String phoneNumber;
 
 }
