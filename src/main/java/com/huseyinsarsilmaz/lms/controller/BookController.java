106bf371fe28c69a52e06c9eab471a1b95145f49
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-05 15:27:07 +0300

    #12 Add book controller and create endpoint

---
+++ src/main/java/com/huseyinsarsilmaz/lms/controller/BookController.java
@@ -0,0 +1,47 @@
+package com.huseyinsarsilmaz.lms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huseyinsarsilmaz.lms.model.dto.request.BookCreateRequest;
+import com.huseyinsarsilmaz.lms.model.dto.response.ApiResponse;
+import com.huseyinsarsilmaz.lms.model.dto.response.BookSimple;
+import com.huseyinsarsilmaz.lms.model.entity.Book;
+import com.huseyinsarsilmaz.lms.model.entity.User;
+import com.huseyinsarsilmaz.lms.service.BookService;
+import com.huseyinsarsilmaz.lms.service.UserService;
+import com.huseyinsarsilmaz.lms.service.Utils;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> createBook(
+            @RequestHeader("Authorization") String token,
+            @Valid @RequestBody BookCreateRequest req) {
+
+        User myUser = userService.getFromToken(token);
+        userService.checkRole(myUser, User.Role.ROLE_LIBRARIAN);
+
+        bookService.isIsbnTaken(req.getIsbn());
+
+        Book newBook = bookService.create(req);
+
+        return Utils.successResponse(Book.class.getSimpleName(), "created", new BookSimple(newBook),
+                HttpStatus.OK);
+    }
+
+}
