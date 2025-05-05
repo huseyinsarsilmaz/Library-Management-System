863261d76e35072828c9be79e0b451d4096e2450
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-05 15:26:10 +0300

    #12 Add isbn checker method to interface

--- src/main/java/com/huseyinsarsilmaz/lms/repository/BookRepository.java
+++ src/main/java/com/huseyinsarsilmaz/lms/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.huseyinsarsilmaz.lms.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.huseyinsarsilmaz.lms.model.entity.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
 
 }
--- src/main/java/com/huseyinsarsilmaz/lms/service/BookService.java
+++ src/main/java/com/huseyinsarsilmaz/lms/service/BookService.java
@@ -4,5 +4,8 @@ import com.huseyinsarsilmaz.lms.model.dto.request.BookCreateRequest;
 import com.huseyinsarsilmaz.lms.model.entity.Book;
 
 public interface BookService {
+
     public Book create(BookCreateRequest req);
+
+    public void isIsbnTaken(String isbn);
 }
