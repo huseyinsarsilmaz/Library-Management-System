ca0ee03e1f819ab05ec6e3e3f3165d51bea806e7
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-07 01:55:21 +0300

    #14 Add search methods in book repository

--- src/main/java/com/huseyinsarsilmaz/lms/repository/BookRepository.java
+++ src/main/java/com/huseyinsarsilmaz/lms/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.huseyinsarsilmaz.lms.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,13 @@ import com.huseyinsarsilmaz.lms.model.entity.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
+
+    Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    Page<Book> findByAuthorContainingIgnoreCase(String author, Pageable pageable);
+
+    Page<Book> findByIsbnContainingIgnoreCase(String isbn, Pageable pageable);
+
+    Page<Book> findByGenre(Book.Genre genre, Pageable pageable);
 
 }
