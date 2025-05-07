56b7e217d4f7a59339597d65b776650cdb6e9009
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-07 17:37:26 +0300

    #17 Add get my borrowing history endpoint

--- src/main/java/com/huseyinsarsilmaz/lms/controller/BorrowingController.java
+++ src/main/java/com/huseyinsarsilmaz/lms/controller/BorrowingController.java
@@ -1,7 +1,11 @@
 package com.huseyinsarsilmaz.lms.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.huseyinsarsilmaz.lms.model.dto.request.BorrowRequest;
 import com.huseyinsarsilmaz.lms.model.dto.response.ApiResponse;
 import com.huseyinsarsilmaz.lms.model.dto.response.BorrowingDetailed;
+import com.huseyinsarsilmaz.lms.model.dto.response.BorrowingHistory;
 import com.huseyinsarsilmaz.lms.model.dto.response.BorrowingSimple;
 import com.huseyinsarsilmaz.lms.model.entity.Borrowing;
 import com.huseyinsarsilmaz.lms.model.entity.User;
@@ -62,6 +67,32 @@ public class BorrowingController {
         borrowing = borrowingService.returnBorrowing(borrowing);
 
         return Utils.successResponse(Borrowing.class.getSimpleName(), "returned", new BorrowingDetailed(borrowing),
+                HttpStatus.OK);
+    }
+
+    private BorrowingHistory getBorrowingHistory(List<Borrowing> borrowings) {
+        List<BorrowingSimple> active = new ArrayList<>();
+        List<BorrowingDetailed> returned = new ArrayList<>();
+
+        for (Borrowing borrowing : borrowings) {
+            if (borrowing.isReturned()) {
+                returned.add(new BorrowingDetailed(borrowing));
+            } else {
+                active.add(new BorrowingSimple(borrowing));
+            }
+        }
+
+        return new BorrowingHistory(active, returned);
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<ApiResponse> getMyBorrowingHistory(@RequestHeader("Authorization") String token) {
+
+        User myUser = userService.getFromToken(token);
+        List<Borrowing> borrowings = borrowingService.getByBorrowerId(myUser.getId());
+        BorrowingHistory borrowingHistory = getBorrowingHistory(borrowings);
+
+        return Utils.successResponse(Borrowing.class.getSimpleName() + " history", "acquired", borrowingHistory,
                 HttpStatus.OK);
     }
 
