9fa36ea829a5b21b68c19876b289ffd490371028
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-07 13:29:02 +0300

    #15 Add borrowing repository

---
+++ src/main/java/com/huseyinsarsilmaz/lms/repository/BorrowingRepository.java
@@ -0,0 +1,11 @@
+package com.huseyinsarsilmaz.lms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.huseyinsarsilmaz.lms.model.entity.Borrowing;
+
+@Repository
+public interface BorrowingRepository extends JpaRepository<Borrowing, Long> {
+
+}
