200399280007fd5c50851e32d9ab7a1c3b893d4c
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-07 13:57:00 +0300

    #15 Remove date to internally manage them in borrow dto

--- src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/BorrowRequest.java
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/BorrowRequest.java
@@ -1,15 +1,7 @@
 package com.huseyinsarsilmaz.lms.model.dto.request;
 
-import java.time.LocalDate;
-
-import com.huseyinsarsilmaz.lms.validation.Alphabethical;
-import com.huseyinsarsilmaz.lms.validation.PhoneNumber;
 import com.huseyinsarsilmaz.lms.validation.RequiredField;
-import com.huseyinsarsilmaz.lms.validation.StrSize;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -22,13 +14,5 @@ public class BorrowRequest {
 
     @RequiredField(entityName = "Book", fieldName = "id")
     private Long bookId;
-
-    @RequiredField(entityName = "Borrow", fieldName = "date")
-    @FutureOrPresent(message = "{fail.date.future.present}")
-    private LocalDate borrowDate;
-
-    @RequiredField(entityName = "Due", fieldName = "date")
-    @Future(message = "{fail.date.future}")
-    private LocalDate dueDate;
 
 }
