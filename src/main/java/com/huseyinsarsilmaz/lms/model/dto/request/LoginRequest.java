3d36a5e410d7489269ccf575eb2d8bd5dd6ec1fe
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-12 00:14:24 +0300

    #24 Add all args constructor to request dtos

--- src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/LoginRequest.java
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/LoginRequest.java
@@ -4,11 +4,13 @@ import com.huseyinsarsilmaz.lms.validation.RequiredField;
 import com.huseyinsarsilmaz.lms.validation.StrSize;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     @RequiredField(entityName = "User", fieldName = "email")
--- src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/RegisterRequest.java
+++ src/main/java/com/huseyinsarsilmaz/lms/model/dto/request/RegisterRequest.java
@@ -6,11 +6,13 @@ import com.huseyinsarsilmaz.lms.validation.RequiredField;
 import com.huseyinsarsilmaz.lms.validation.StrSize;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
 
     @Email(message = "{fail.email.format}")
