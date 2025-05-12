894eaeee37886e4738b3f8092ce697d79520d869
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-05-13 01:39:01 +0300

    #26 Remove reactive implementations and adjust pom xml

--- src/main/java/com/huseyinsarsilmaz/lms/service/ReactiveBookService.java
+++
@@ -1,9 +0,0 @@
-package com.huseyinsarsilmaz.lms.service;
-
-import com.huseyinsarsilmaz.lms.model.entity.Book;
-
-import reactor.core.publisher.Mono;
-
-public interface ReactiveBookService {
-    Mono<Void> updateAvailability(Book book, boolean available);
-}
